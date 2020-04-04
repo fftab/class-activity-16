486019ce62f81be4066f1609d097e01b4f7521dc
Florencia Tabarez <fftabarez97@gmail.com>
2020-04-04 17:57:56 -0600

    Structured HTML created variables to contain input values, created functions to query the API and act on response data.

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
 // Global Variables
 // ===================================
 
+let searchTerm = $("#search-term").val().trim();
+let startYear = $("#start-year").val().trim();
+let endYear = $("#end-year").val().trim();
 
 // Functions
 // ====================================
-$("#add-search-terms").on("click"), function() {
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=T7PcwJpFJq4GepMWCLX6jEtqtUyagCem";
-    
-    
+function createQueryUrl() {
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+    let queryParams = {
+        "api-key": "T7PcwJpFJq4GepMWCLX6jEtqtUyagCem"
     };
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-            }).then(function(response) {
+
+    queryParams.q = searchTerm;
+    
+    if (parseInt(startYear)) {
+
+        // push startYear as property into queryParams
+        queryParams.begin_date = startYear + "0101";
+
+    }
+
+    if (parseInt(endYear)) {
+
+        queryParams.end_date = endYear + "0101";
+
+    }
+
+    return queryURL + $.params(queryParams)
+
+}
+
+function updatePage(whateverYouWant) {
+
+    // 
+    let articleCount = $("#article-count").val();
+
+    for (i = 0; i < articleCount; i++) {
+
+        // grabbing the article quantity form NYTTimes
+
+        let article = whateverYouWant.resonse.docs[i];
+
+        let numArticles = i + 1;
+
+        
+
+    }
+
+}
+
+// $("#add-search-terms").on("click"), function() {
+    
+//     $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//         }).then(function(response) {
           
-            });
+//             });
             
-            console.log(response);
+//             console.log(response);
     
     
+//     };
+
+    
+
     
     
